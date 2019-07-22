bbca213eb9b1425a18351e113a14ad7520a7dd02
JaebeomKwon <jaebumi0415@naver.com>
2019-07-22 15:52:16 +0900

    main.html 파일생성 TestController.java 안에 추가

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	@RequestMapping("/board")
+	public String board() {
+		return "board";
+	}
+	
+	
+}
